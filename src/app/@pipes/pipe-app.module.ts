f015bec070512c4ae12dc3db733f3e23b35876c3
leoQm <prueba@hotmail.com>
2024-07-10 18:06:18 -0500

    servicis trabajador empleador imlementado

--- src/app/@pipes/pipe-app.module.ts
+++ src/app/@pipes/pipe-app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PipeCapitalizePipe } from './pipe-capitalize.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PipeCapitalizePipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PipeCapitalizePipe
   ]
 })
 export class PipeAppModule { }
